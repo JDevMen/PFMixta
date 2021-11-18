5ed4bb68ad5676e49eb7b74f9cce6d8482c20cb4
Julian Mendoza Ruiz <jdmendoza_90@hotmail.com>
2021-11-18 14:33:24 -0500

    cambio de nomenclatura

--- Assets/Scripts/copyCatScript.cs
+++ Assets/Scripts/copyCatScript.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class copyCatScript : MonoBehaviour
 {
-    public GameObject camara;
     public GameObject cabeza;
+    public GameObject camara;
 
     // Update is called once per frame
     void Update()
     {
-        float posY = cabeza.transform.position.y;
-        float posX = camara.transform.position.x;
-        float posZ = camara.transform.position.z;
+        float posY = camara.transform.position.y;
+        float posX = cabeza.transform.position.x;
+        float posZ = cabeza.transform.position.z;
         Vector3 newPos = new Vector3(posX, posY, posZ);
 
-        camara.transform.position = newPos;
+        cabeza.transform.position = newPos;
     }
 }

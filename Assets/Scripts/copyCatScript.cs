e466bc81d71ce8ed4fce0e01c2cd089f63b755e9
Julian Mendoza Ruiz <jdmendoza_90@hotmail.com>
2021-11-18 13:20:58 -0500

    copycat

---
+++ Assets/Scripts/copyCatScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class copyCatScript : MonoBehaviour
+{
+
+    public GameObject cabeza;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float posY = cabeza.transform.position.y;
+        float posX = transform.position.x;
+        float posZ = transform.position.z;
+        Vector3 newPos = new Vector3(posX, posY, posZ);
+    }
+}

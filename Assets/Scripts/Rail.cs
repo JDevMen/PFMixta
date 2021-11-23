b2469960415f9f7cde7b7e696a93d07d4a6bb081
Julian Mendoza Ruiz <jdmendoza_90@hotmail.com>
2021-11-23 16:41:14 -0500

    adelanto

--- Assets/Scripts/Rail.cs
+++ Assets/Scripts/Rail.cs
@@ -20,6 +20,38 @@ public class Rail : MonoBehaviour
         return Vector3.Lerp(pos1, pos2, ratio);
     }
 
+    public Vector3 CatmullPos(int seg, float ratio)
+    {
+        Vector3 p1, p2, p3, p4;
+
+        if (seg == 0)
+        {
+            p1 = nodes[seg].position;
+            p2 = p1;
+            p3 = nodes[seg + 1].position;
+            p4 = nodes[seg + 2].position;
+        }
+        else if (seg == nodes.Length - 2)
+        {
+            p1 = nodes[seg - 1].position;
+            p2 = nodes[seg].position;
+            p3 = nodes[seg + 1].position;
+            p4 = p3;
+        }
+        else
+        {
+            p1 = nodes[seg - 1].position;
+            p2 = nodes[seg].position;
+            p3 = nodes[seg + 1].position;
+            p4 = nodes[seg + 2].position;
+        }
+
+        float t2 = ratio * ratio;
+        float t3 = t2 * ratio;
+
+        float x = 0.5f * ((2.0f * p2.x) + (-p1.x + p3.x) * t2 + (2.0f * p1.x - 5.0f * p2.x + 4 * p3.x - p4.x) * t2 + (-p1.x + 3.0f *));
+    }
+
     public Quaternion Orientation(int seg, float ratio)
     {
         Quaternion q1 = nodes[seg].rotation;
@@ -27,6 +59,8 @@ public class Rail : MonoBehaviour
 
         return Quaternion.Lerp(q1, q2, ratio);
     }
+
+
 
     private void OnDrawGizmos()
     {

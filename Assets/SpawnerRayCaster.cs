f4ccb47e21d08044b50507f913972ff6b90eacec
diegommezp <da.gomezp@uniandes.edu.co>
2021-11-23 16:26:15 -0500

    things

--- Assets/SpawnerRayCaster.cs
+++ Assets/SpawnerRayCaster.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class SpawnerRayCaster : MonoBehaviour
 {
@@ -9,16 +10,27 @@ public class SpawnerRayCaster : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // var inputDevices = new List<UnityEngine.XR.InputDevice>();
-        // UnityEngine.XR.InputDevices.GetDevices(inputDevices);
-        // Debug.Log(inputDevices);
-        // UnityEngine.XR.Input
-        // foreach (var device in inputDevices)
-        // {
-        //     Debug.Log(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.role.ToString()));
-        // }
+        var inputDevices = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevices.GetDevices(inputDevices);
+        Debug.Log("objeto " + inputDevices.ToString());
+        foreach (var device in inputDevices)
+        {
+            Debug.Log(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.role.ToString()));
+        }
         // Input.GetAxis("Right Controller Trigger");
-        Input.GetButton("Primary (sandwich button)(1)");
+        // Input.GetButton("");
+        var leftHandDevices = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.LeftHand, leftHandDevices);
+
+        if(leftHandDevices.Count == 1)
+        {
+            UnityEngine.XR.InputDevice device = leftHandDevices[0];
+            Debug.Log(string.Format("Device name '{0}' with role '{1}'", device.name, device.role.ToString()));
+        }
+        else if(leftHandDevices.Count > 1)
+        {
+            Debug.Log("Found more than one left hand!");
+        }
     }
 
     // Update is called once per frame
